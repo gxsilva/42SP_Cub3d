0a84b4ce45787a678b3347556b4807b67c7242d3
gxsilva <lssvgabriel@gmail.com>
2025-07-09 20:19:47 -0300

    fix(leak): Handle with texture leaks

--- src/free/free_cube.c
+++ src/free/free_cube.c
@@ -6,7 +6,7 @@
 /*   By: lsilva-x <lsilva-x@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 18:13:39 by lsilva-x          #+#    #+#             */
-/*   Updated: 2025/07/01 02:20:11 by lsilva-x         ###   ########.fr       */
+/*   Updated: 2025/07/09 20:18:42 by lsilva-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,29 @@ void	free_cube(t_cube *cube);
 void	free_file(t_file *file);
 void	free_map(t_map *map);
 void	free_player(t_player *player);
+void	free_textures(t_textures *textures);
 
 void	free_cube(t_cube *cube)
 {
 	free_file(cube->file);
 	free_map(cube->map);
 	free_player(cube->player);
+	free_textures(cube->textures);
+}
+
+void	free_textures(t_textures *textures)
+{
+	if (!textures)
+		return ;
+	if (textures->north)
+		mlx_delete_texture(textures->north);
+	if (textures->south)
+		mlx_delete_texture(textures->south);
+	if (textures->east)
+		mlx_delete_texture(textures->east);
+	if (textures->west)
+		mlx_delete_texture(textures->west);
+	free(textures);
 }
 
 void	free_file(t_file *file)
